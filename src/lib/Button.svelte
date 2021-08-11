09041abbdd78bc74acb185894b2b3b34d372d65b
Minh-ctrl <binhminh140899@gmail.com>
2021-08-11 22:46:55 +0900

    finished random button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,4 +1,9 @@
 <script>
-
+    export let disabled = false;
 </script>
-<button class="w-20 h-10" ><slot/></button>
+
+
+<button on:click class="bg-transparent hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-15 h-10 hover:border-transparent mt-5" {disabled}>
+    <slot/>
+</button>
+
--- src/lib/Color_box.svelte
+++ src/lib/Color_box.svelte
@@ -1,7 +1,7 @@
 <script>
-    export let default_color=200;
+    export let color_hue;
     export let steps;
-    $:actual_value= +default_color + +steps;
+    $:actual_value= +color_hue + +steps;
 </script>
 
 <div class="border-solid border-4 shadow-2xl w-1/6 h-2/6 rounded-3xl" style="background: linear-gradient(175deg, hsl({actual_value}, 60%,70%) 0%, hsl({actual_value}, 30%,30%) 100% )"></div>
